caa28273c198ca2fbec74cfa1df300532f8ed9f6
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-18 20:21:34 +0100

    basic test

--- test/basic.js
+++ test/basic.js
@@ -1,10 +1,18 @@
 var Packet = require('../lib/collectd').Packet;
 
-exports.testParts = function(test) {
+exports.testInt16 = function(test) {
 	test.expect(2);
 	var p = new Packet();
 	p.appendInt16(42);
 	test.equals(2, p.length);
 	test.equals(new Buffer([0, 42 ]).toString(), p.asBuffer().toString());
 	test.done();
+};
+
+exports.testString = function(test) {
+	test.expect(2);
+	var p = new Packet();
+	p.appendString('toto');
+	test.equals(4+7, p.length);
+	console.log(p.asBuffer());
 };
