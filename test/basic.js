21c592e595db0d6bbd2ffbe8e015461891b65b4b
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-18 20:35:37 +0100

    asArray

--- test/basic.js
+++ test/basic.js
@@ -1,11 +1,19 @@
 var Packet = require('../lib/collectd').Packet;
 
+Buffer.prototype.asArray = function() {
+	var a = new Array(this.length);
+	for(var i = 0; i < this.length; i++) {
+		a[i] = this[i];
+	}
+	return a;
+};
+
 exports.testInt16 = function(test) {
 	test.expect(2);
 	var p = new Packet();
 	p.appendInt16(42);
 	test.equals(2, p.length);
-	test.equals(new Buffer([0, 42 ]).toString(), p.asBuffer().toString());
+	test.deepEqual([0, 42], p.asBuffer().asArray());
 	test.done();
 };
 
@@ -13,6 +21,7 @@ exports.testString = function(test) {
 	test.expect(2);
 	var p = new Packet();
 	p.appendString('toto');
-	test.equals(4+7, p.length);
-	console.log(p.asBuffer());
+	test.equals(4+3, p.length);
+	test.deepEqual([0, 4+3+2, 116, 111, 116, 111, 0], p.asBuffer().asArray());
+	test.done();
 };
